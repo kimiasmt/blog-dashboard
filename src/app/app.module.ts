7d24a290bb10e76c417caa20d8b9967185538763
Kimia Sameti <36294855+kimiasmt@users.noreply.github.com>
2022-08-26 13:47:04 +0430

    login html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { LoginComponent } from './pages/authentication/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
