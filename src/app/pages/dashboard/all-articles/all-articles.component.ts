04b7b716efe37290d5a806d9fc80209603523392
Kimia Sameti <36294855+kimiasmt@users.noreply.github.com>
2022-08-29 19:35:55 +0430

    create all articles table

---
+++ src/app/pages/dashboard/all-articles/all-articles.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from '../../../core/services/articles.service';
+
+@Component({
+  selector: 'app-all-articles',
+  templateUrl: './all-articles.component.html',
+  styleUrls: ['./all-articles.component.scss']
+})
+export class AllArticlesComponent implements OnInit {
+
+  constructor(private articlesService: ArticlesService) { }
+  articles: any ;
+
+  getArticle () {
+    this.articlesService.getArticles().subscribe((res:any) => {
+      this.articles = res?.articles;
+    })
+  }
+
+  ngOnInit(): void {
+    this.getArticle()
+    // console.log(this.getArticle());
+  }
+
+}
