4671637394d93a942943115fd984323e305abd9e
Kimia Sameti <36294855+kimiasmt@users.noreply.github.com>
2022-08-31 13:03:18 +0430

    Implement guard for dashboard for nonlogin users

--- src/app/core/guard/authentication.guard.ts
+++ src/app/core/guard/authentication.guard.ts
@@ -1,34 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthenticationService,
     private router: Router
   ) {
-    console.log('ihihihihih');
   }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    console.log('outer');
-
-    if (!this.authService.isLogin()) {
-      console.log('inner');
-      this.router.navigate(['/login']).then();
-    }
-    console.log('outer');
-
-    return true;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
+      if (!this.authService.isLogin()) {
+        this.router.navigate(['/login']).then();
+      }
+      return true;
   }
 }
